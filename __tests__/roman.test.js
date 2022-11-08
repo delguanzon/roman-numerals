87edb302bb183d0dd692fde7f16cee2809cf56a4
delguanzon <yodelguanzon@gmail.com>
2022-11-08 14:47:14 -0800

    Pass: add method for translating 1 to I
    Co-authored-by: VW <vera.weikel@gmail.com>

--- __tests__/roman.test.js
+++ __tests__/roman.test.js
@@ -6,9 +6,10 @@ describe("RomanNum", () => {
     let romanNum = new RomanNum(123);
     expect(romanNum.currVal).toEqual(123);
   });
-});
 
   test('should return "I" if the value passed is 1', () => {
     let num = new RomanNum(1);
     expect(num.toRoman()).toEqual('I');
   });
+
+});
--- src/roman.js
+++ src/roman.js
@@ -2,3 +2,10 @@ export default function RomanNum(number){
   this.currVal = number;
   return this.currVal;  
 }
+
+RomanNum.prototype.toRoman = function (){
+  if(this.currVal === 1){
+    return 'I';
+  }
+  return 0;
+};
